4d88920f91e0d01b11ea371f2749a828e8c17545
MaelleDaniel <maelledaniel2@gmail.com>
2021-03-01 15:17:32 +0100

    maj appearence

--- backend/routes/article.js
+++ backend/routes/article.js
@@ -1,38 +1,40 @@
 // j'appelle express
-const express = require('express'); 
-const router= express.Router();
+const express = require("express");
+const router = express.Router();
 
 //j'indique que mon routeur doit utiliser le modèle mongoose qui est dans article.js dans models
-const article= require('../models/article');
+const article = require("../models/article");
 
 // Toutes ce qui suit et qui commence par "router" sont des middlewares, ça indique au serveur que faire en fonction des requètes
 // qu'il reçoit de l'utilisateur. Elles sont en fonction asynchrones ce qui veut dire qu'on lui dit "essaye ça" (try) et "si t'arrive
 //pas tu fais ça" (catch).
 
 //c'est la route "principale", on dit "si on te demande ça, tu fais ça".
-router.get('/',async (req, res)=>{
-    try{
-        //mais c'est quoi le "ça" qu'il doit faire?
-        //on lui dit d'aller chercher le contenu intégral de la bdd
-        const arts = await article.find();
-        // et de l'envoyer au front
-        res.send(arts);    
-    }catch (err){
-        //Si ça marche pas on envoie une erreur dans la console
-        //on pourrait aussi rediriger vers une page en écrivant un truc du genre res.redirect("la page de redirection.html")
-        res.jsons({message:error});}
+router.get("/", async (req, res) => {
+  try {
+    //mais c'est quoi le "ça" qu'il doit faire?
+    //on lui dit d'aller chercher le contenu intégral de la bdd
+    const arts = await article.find();
+    // et de l'envoyer au front
+    res.send(arts);
+  } catch (err) {
+    //Si ça marche pas on envoie une erreur dans la console
+    //on pourrait aussi rediriger vers une page en écrivant un truc du genre res.redirect("la page de redirection.html")
+    res.jsons({ message: error });
+  }
 });
 
 // c'est la route qui nous permet d'aller chercher les articles individuellement
-router.get('/:articleId', async (req, res) => {
-    try{
-        //on luit dit quand on te demande un article par son ID, tu vas le chercher dans la bdd
-        const art = await article.findById(req.params.articleId);
-        //et tu le renvois en json pour que le js du front s'en serve
-        res.json(art);
-    }catch(err){
-        //ici ça va toujours être le même principe: ça foire=erreur
-        res.json({message:error});}
+router.get("/:articleId", async (req, res) => {
+  try {
+    //on lui dit quand on te demande un article par son ID, tu vas le chercher dans la bdd
+    const art = await article.findById(req.params.articleId);
+    //et tu le renvois en json pour que le js du front s'en serve
+    res.json(art);
+  } catch (err) {
+    //ici ça va toujours être le même principe: ça foire=erreur
+    res.json({ message: error });
+  }
 });
 
 // //c'est la route qui doit permettre de supprimer un article,
@@ -56,22 +58,23 @@ router.get('/:articleId', async (req, res) => {
 //     }
 // });
 
-
 // c'est la route qui indique la marche à suivre quand le serveur reçoit un formulaire
-router.post('/', async(req,res) =>{
-    //on lui indique la "tête" des données (bodyparser fait son action ici)
-    const art= new article({
-        titre: req.body.titre,
-        auteur: req.body.auteur,
-        createdAt: new Date(),
-        article: req.body.article});
-    try{
+router.post("/", async (req, res) => {
+  //on lui indique la "tête" des données (bodyparser fait son action ici)
+  const art = new article({
+    titre: req.body.titre,
+    auteur: req.body.auteur,
+    createdAt: new Date(),
+    article: req.body.article,
+  });
+  try {
     //on lui dit de charger tout ça dans la bdd
-        await art.save();
-        //et de nous rediriger vers une page HTML fixe
-        res.redirect("accepted.html");
-    }catch(err){
-        res.json({message :error});}
+    await art.save();
+    //et de nous rediriger vers une page HTML fixe
+    res.redirect("accepted.html");
+  } catch (err) {
+    res.json({ message: error });
+  }
 });
 
 //on exporte le router pour qu'il puisse être utilisé autre part (app.js)
--- frontend/index.js
+++ frontend/index.js
@@ -1,93 +1,112 @@
-//petite fonction très simple permettant de formater la date 
 const dateFormatter = new Intl.DateTimeFormat("fr", {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",});
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+});
 
-//construction de la fonction qui permet de récupérer les données dans la bdd
 const getArts = async () => {
-    //le fetch va chercher les infos d'une base de données, on fetch à l'adresse de notre serveur parce que tout passe par lui
-    //quand il reçoit cette requête le serveur passe par sa route 'get'
-    const response = await fetch("http://localhost:3000/article");
-    //on récupère ces données en json
-    const article = await response.json();
-    // et on les console.log pour être sure qu'elles sont là (étape de travail, ce n'est pas nécessaire dans un produit fini)
-    console.log(article);
-    // on appelle la fonction resetPage (aller la voir) elle ne sert pas à la première visite du sommaire mais elle sert après
-    resetPage();
-    //on appelle la fonction generateHtml appliquée aux données Json qu'on a réccupérer
-    generateHtml(article);};
+  const response = await fetch("http://localhost:3000/article");
 
-//cette fonction permet de générer du html là où on le voudra dans une div du html
+  const article = await response.json();
+
+  console.log(article);
+
+  resetPage();
+
+  generateHtml(article);
+};
+
 const generateHtml = (data) => {
-    //on créer une variable string vide en base de travail
-    let html = '';
-    //afin d'afficher tous les articles, on créer une boucle for
-    //on créer une variable i (convention pour les boucles for), le deuxième élément dans la parenthèse indique que tant que i n'est
-    //pas suppérieur au nombre de données, la boucle reprend (donc dans notre cas tant qu'on a pas tout affiché), et i++ indique que
-    //tant que la boucle tourne on rajoute 1 à i et que donc pour notre cas, on passe à l'article suivant dans notre base de données)
-    for (let i = data.length-1 ; i >0; i--) {
-        //on indique le contenu du html que l'on va intégrer à notre page html de base
-        //ne pas oublier le [i], ça marche pas sans ;)
-        //petit focus sur la dernière ligne (le boutton), on y appelle un fonction au click (onclick) et on lui injecte en argument l'id
-        // de l'article qui nous intéresse, ce sera important pour la suite
-        html += `<div class="articles">
+  let title = `<h1>Sommaire</h1>`;
+  const titlediv = document.querySelector("#title");
+  titlediv.innerHTML = title;
+
+  let html = "";
+
+  for (let i = data.length - 1; i >= 0; i--) {
+    html += `
+    <div class="articles">
         <div><h3>${data[i].titre}</h3></div>
-        <div>by: ${data[i].auteur}</div>
-        <div class="date">${dateFormatter.format(new Date(data[i].createdAt))}</div>
-        <button type="button" onclick="getArticle('${data[i]._id}')">AFFICHER</button>
-        </div>`
+        <div>Par: ${data[i].auteur}</div>
+        <div class="date">${dateFormatter.format(
+          new Date(data[i].createdAt)
+        )}</div>
+        <button type="button" onclick="getArticle('${
+          data[i]._id
+        }')">AFFICHER</button>
+        </div>`;
 
-        // je sélectionne l'endroit dans mon fichier html où je vais implémenter tout ça 
-        const artdiv = document.querySelector('#test')
-        // et je dis à ma fonction de l'injecter quand elle est appelé
-        artdiv.innerHTML = html}};
+    const artdiv = document.querySelector("#corps");
 
-    
-//cette fonction permet d'effacer le html qu'on a généré automatiquement
+    artdiv.innerHTML = html;
+  }
+};
+
 const resetPage = () => {
-    //on sélectionne l'endroit qui nous intéresse et on inject un string vide à la place du html généré
-    document.querySelector("#test").innerHTML = "";
-  };
+  document.querySelector("#title").innerHTML = "";
+  document.querySelector("#corps").innerHTML = "";
+};
 
-// cette fonction permet d'aller chercher et de générer l'affichage d'un seul article. 
-//on a en argument (entre les parenthèses) l'id de l'article qu'on chercher
 const getArticle = async (id) => {
-    // pour avoir le bon chemin vers l'article on créer donc une variable qui concatène l'id mis en argument dans l'URL
-    let Url=`http://localhost:3000/article/${id}`
-    // on va donc "fetcher" les données de cet article exactement de la meme manière que quand on appelle toute la bdd
-    const response = await fetch(Url);
-    // on demande la chose sous forme de json
-    const post = await response.json();
-    //on rafraichi la page grace la fonction resetPage
-    resetPage();
-    // et on génère le html pour l'article spécifique
-    generateHtmlArticle (post);}
+  let Url = `http://localhost:3000/article/${id}`;
 
-//fonction qui génère le html d'un article précis
-const generateHtmlArticle= (post) =>{
-    //c'est exactement le même principe que le premier generate html sauf qu'on a pas besoin de la boucle for
-    // c'est donc simple, on dispose nos données dans des balises html (qui peuvent avoir des class et des id et qui seront
-    //pris en charge par le css ;) )
-    const html= `<div>
-    <div class="info"><h1>${post.titre}</h1></div>
-    <div class="info">by: ${post.auteur}</div>
+  const response = await fetch(Url);
+
+  const post = await response.json();
+
+  resetPage();
+
+  generateHtmlArticle(post);
+};
+
+const generateHtmlArticle = (post) => {
+  const html = `<div class="articles solo">
+    <div class="info"><h2>${post.titre}</h2></div>
+    <div class="info">Par: ${post.auteur}</div>
     <div class="info">${dateFormatter.format(new Date(post.createdAt))}</div>
-    <div id="article">${post.article}</div>
+    <div id="article-main">${post.article}</div>
     <button type="button" onclick="getArts()">Retour au Sommaire</button>
-    </div>`
-    //on sélectionne là où on veut le mettre
-    const infodiv = document.querySelector('#test')
-    //et on lui injecte
-        infodiv.innerHTML = html};
+    </div>`;
 
+  const infodiv = document.querySelector("#corps");
 
-const Accepted=async()=>{
-    resetPage();
-    const acceptedHtml=`<p>Votre formulaire a été accepté dans notre base de données</p>
-    <button onclick="getArts()" >Retour au Sommaire</button>`
-    const Accepted = document.querySelector('#test')
-        Accepted.innerHTML =acceptedHtml;
-}
-// je met en route ma fonction getArts
- getArts();
+  infodiv.innerHTML = html;
+};
+
+const Accepted = async () => {
+  resetPage();
+  const acceptedHtml = `<p>Votre formulaire a été accepté dans notre base de données</p>
+    <button onclick="getArts()" >Retour au Sommaire</button>`;
+  const Accepted = document.querySelector("#test");
+  Accepted.innerHTML = acceptedHtml;
+};
+
+const Post = () => {
+  resetPage();
+  let title = `<h1>Poster un article</h1>`;
+  const titlediv = document.querySelector("#title");
+  titlediv.innerHTML = title;
+
+  html = `
+            <form id="form1" method="post" action="http://localhost:3000/article">
+                <div class="label">
+                    <label for="titre">Titre de l'article :</label>
+                    <input type="text" name="titre" id="titre" placeholder="Titre de l'article" required/>
+                </div>
+                <div class="label">
+                    <label for="auteur">Nom de l'auteur:</label>
+                    <input type="text" name="auteur" id="auteur" placeholder="Nom de l'auteur"required/>
+                </div>
+                <div class="label">
+                    <label for="article">Votre article :</label>
+                    <textarea name="article" id="article" rows="10" cols="50" placeholder="Parlez nous de votre ressource" required></textarea>
+                </div>
+                <div>
+                <input class="button-primary" type="submit" value="Envoyer">
+                </div>
+            </form>`;
+  const form = document.querySelector("#corps");
+  form.innerHTML = html;
+};
+
+getArts();
